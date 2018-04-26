8f9ab2d2b3e0eea82cb07a63274b835cfcfda664
Nick Milazzo <nmils820@gmail.com>
2018-04-26 12:50:37 -0400

    Setup basic structure and injecting js code correctly

---
+++ index.js
@@ -0,0 +1,5 @@
+// const Sentiment = require('sentiment');
+// const sentiment = new Sentiment();
+// const result = sentiment.analyze('Cats are stupid.');
+// console.dir(`result :`, result);
+console.log(`is this script being injected????`);
