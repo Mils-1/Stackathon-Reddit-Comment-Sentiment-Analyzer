a7de142d3f858cea232b6dfb672c089090f86b7b
Nick Milazzo <nmils820@gmail.com>
2018-04-26 20:14:33 -0400

    Added additional summarization data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
 const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
 
-const result = sentiment.analyze('Cats are stupid.');
-console.log(`result of sentiment.analyze('Cats are stupid.') :`, result);
-
 const commentNodeList = document.querySelectorAll('div.entry p');
 const commentArr = Array.from(commentNodeList);
 const filteredCommentArr = commentArr.filter(comment => {
   return comment.className.trim() !== 'tagline';
 })
+
 console.log(`filteredCommentArr`);
 console.dir(filteredCommentArr);
+
 const sentimentDataArr = filteredCommentArr.map(comment => {
   return sentiment.analyze(comment.innerText);
 });
+
 console.log(`sentimentDataArr`);
 console.dir(sentimentDataArr);
-// console.log(`--------------`);
-// console.log(`textArr`);
-// console.dir(textArr);
+
+const convertRawSentimentDataToSummarizedData = (arr) => {
+  const totalScore = arr.reduce((acc, curr) => {
+    return acc + curr.score;
+  }, 0);
+  const averageScore = totalScore / arr.length;
+  const positiveTokenObj = {};
+  const negativeTokenObj = {};
+  arr.forEach(comment => {
+    comment.positive.forEach(token => {
+      if (positiveTokenObj[token]) {
+        positiveTokenObj[token]++;
+      } else {
+        positiveTokenObj[token] = 1;
+      }
+    });
+    comment.negative.forEach(token => {
+      if (negativeTokenObj[token]) {
+        negativeTokenObj[token]++;
+      } else {
+        negativeTokenObj[token] = 1;
+      }
+    });
+  })
+  return {
+    totalScore,
+    averageScore,
+    positiveTokenObj,
+    negativeTokenObj
+  }
+}
+
+console.log(`######## convertedData ########`);
+console.log(convertRawSentimentDataToSummarizedData(sentimentDataArr));
 
